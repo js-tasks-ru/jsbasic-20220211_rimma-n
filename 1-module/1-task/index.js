9273bbfc720a11c9fbebb3708cd50d51595ca290
Rimma <rshagimuratova@gmail.com>
2022-02-16 12:20:45 -0600

    решение факториал числа Ñn через цикл for

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -1,3 +1,14 @@
 function factorial(n) {
-  // ваш код...
+  let result = 1;
+	for (let i=0; i<n; i++){
+		result = result *(n-i);
+	}
+	return result;
 }
+console.log(factorial(1));
+console.log(factorial(2));
+console.log(factorial(3));
+console.log(factorial(4));
+console.log(factorial(5));
+console.log(factorial(6));
+console.log(factorial(7));

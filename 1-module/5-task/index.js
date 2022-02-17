9a74cdc022d16718b0bfa2236ed6197ad625ca32
Rimma <rshagimuratova@gmail.com>
2022-02-17 16:02:10 -0600

    усечение строки 1-module/5-task

--- 1-module/5-task/index.js
+++ 1-module/5-task/index.js
@@ -1,3 +1,9 @@
-function truncate(str, maxlength) {
-  // ваш код...
-}
+function truncate(str, maxlength) {	
+	let result;
+		if (str.length > maxlength) {
+			return result = str.slice(0, maxlength - 1) + "…";
+		} else {
+			return result = str;
+		}
+}	
+	

931509c7757bde5e9721894c2c5642ab19f803fb
Rimma <rshagimuratova@gmail.com>
2022-02-17 13:33:14 -0600

    Проверка на спам 1-module/4-task

--- 1-module/4-task/index.js
+++ 1-module/4-task/index.js
@@ -1,3 +1,13 @@
 function checkSpam(str) {
-  // ваш код...
+	let s1 = '1xBet';
+	let s2 = 'XXX';
+	let res = null;
+	s1 = s1.toUpperCase();
+	s2 = s2.toUpperCase();
+	str = str.toUpperCase(); 
+	res = str.includes(s1)||str.includes(s2);
+	
+	return res;
 }
+
+checkSpam(str);

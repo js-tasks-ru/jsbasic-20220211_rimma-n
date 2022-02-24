d84abdc416150e83e55c22b74f99623b8e5833be
Rimma Shagimuratova <rshagimuratova@gmail.com>
2022-02-24 16:51:08 -0600

    решение 3-modul/3-task(Переведите текст вида..)

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -1,3 +1,10 @@
 function camelize(str) {
-  // ваш код...
+let arr = new Array(); 
+arr = str.split("");
+	for(let i=0; i<arr.length; i++){
+		if (arr[i]==="-"){
+			arr[i+1]=arr[i+1].toUpperCase();
+		}	
+	}
+return arr.filter(function(item) { return item!== '-' }).join('');
 }

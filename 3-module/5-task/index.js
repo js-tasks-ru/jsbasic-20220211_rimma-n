fc8037c79b7a84f9332cd031742dc230cbfca9a1
Rimma Shagimuratova <rshagimuratova@gmail.com>
2022-02-26 00:50:55 -0600

    решение 3-modul/5-task(Найти min/max в строке)

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -1,3 +1,11 @@
 function getMinMax(str) {
-  // ваш код...
+str = str.split(' ');
+str = str
+     .map(num => num = Number(num))
+	 .filter(num1 => (!isNaN(num1))&&(isFinite(num1)));
+		
+let min = Math.min.apply(null, str);
+let max = Math.max.apply(null, str);		
+		return str = {min,max};
+
 }

04f4c1ec3c399bcbf356f1bb8eec33350dc02f7d
Rimma Shagimuratova <rshagimuratova@gmail.com>
2022-03-06 22:58:38 -0600

    решение 4-modul/1-task(Функция makeFriendsList)

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -1,3 +1,13 @@
 function makeFriendsList(friends) {
-  // ваш код...
-}
+let arr = friends.map(user => user.firstName + ' ' + user.lastName);	
+let ul = document.createElement('ul');
+
+	for (let i=0; i<arr.length; i++){
+		let li = document.createElement('li');	
+		li.innerHTML = arr[i];
+	    ul.appendChild(li);	 
+    	};	 
+    return ul;
+};
+
+
